29e2fd18bdbcebe482d0dab07bed9a91b38da8fc
Lucas Brandstaetter <lucas@brandstaetter.tech>
2021-03-16 20:32:14 +0100

    Add 2020 d3 p1 solution

--- src/day3.rs
+++ src/day3.rs
@@ -12,3 +12,21 @@ pub fn input_generator(input: &str) -> Vec<Vec<char>> {
   }*/
   return toboggan_map;
 }
+
+#[aoc(day3, part1)]
+pub fn solve_part1(toboggan_map: &Vec<Vec<char>>) -> u32 {
+  let x = 3;
+  let y = 1;
+  let line_length = toboggan_map[0].len();
+  let mut trees = 0;
+  let mut current_x = 0;
+  let mut current_y = 0;
+  while current_y < toboggan_map.len() {
+    if toboggan_map[current_y][current_x] == '#' {
+      trees += 1
+    }
+    current_x = (current_x + x) % line_length;
+    current_y += y;
+  }
+  return trees;
+}
