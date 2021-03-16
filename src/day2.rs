bb352c3df61e4675c16890e057a59e2f9511f1c0
Lucas Brandstaetter <lucas@brandstaetter.tech>
2021-03-16 19:27:15 +0100

    Ignore input

--- src/day2.rs
+++ src/day2.rs
@@ -2,25 +2,27 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 pub struct PwCheck {
-  min: String,
-  max: String,
-  character: String,
-  password: String
+  min: u32,
+  max: u32,
+  character: char,
+  password: String,
 }
 
 #[aoc_generator(day2)]
 pub fn input_generator(input: &str) -> Vec<PwCheck> {
-
-  input.lines().map(|l| {
-    lazy_static! {
-      static ref RE: Regex = Regex::new(r"^(\d+)-(\d+) ([[:alpha:]]): (\w*)$").unwrap();
-    }
-    let cap = RE.captures(l).unwrap();
-    PwCheck {
-      min: cap[1].to_string(),
-      max: cap[2].to_string(),
-      character: cap[3].to_string(),
-      password: cap[4].to_string()
-    }}
-  ).collect()
+  input
+    .lines()
+    .map(|l| {
+      lazy_static! {
+        static ref RE: Regex = Regex::new(r"^(\d+)-(\d+) ([[:alpha:]]): (\w*)$").unwrap();
+      }
+      let cap = RE.captures(l).unwrap();
+      PwCheck {
+        min: cap[1].parse::<u32>().unwrap(),
+        max: cap[2].parse::<u32>().unwrap(),
+        character: cap[3].chars().nth(0).unwrap(),
+        password: cap[4].to_string(),
+      }
+    })
+    .collect()
 }
