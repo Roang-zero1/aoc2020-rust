10f773583b8377f413b8812395f5b523d93f837d
Lucas Brandstaetter <lucas@brandstaetter.tech>
2021-03-16 23:20:46 +0100

    Rename days

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ extern crate aoc_runner;
 extern crate aoc_runner_derive;
 extern crate lazy_static;
 
-pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
 
 aoc_lib! { year = 2020 }
